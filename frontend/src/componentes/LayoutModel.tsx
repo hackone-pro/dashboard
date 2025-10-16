0cf755bae1fa87c5c93ae2cbd859a834cf62520d
Roberto <roberto@maiolidesign.com>
2025-10-16 15:25:38 -0300

    Atualização de versão dashboard

--- frontend/src/componentes/LayoutModel.tsx
+++ frontend/src/componentes/LayoutModel.tsx
@@ -107,6 +107,12 @@ export default function LayoutModel({ children, titulo }: LayoutModelProps) {
 
         {/* Conteúdo da página */}
         {children}
+
+        {/* Rodapé */}
+        <footer className="text-right text-gray-500 text-xs mt-4">
+          Versão 1.3.0.1
+        </footer>
+
       </div>
     </div>
   );
