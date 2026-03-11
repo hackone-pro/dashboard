8792d9805226f4dcbf658fba3ac9f82fc2b3a751
Roberto <roberto@maiolidesign.com>
2026-03-11 01:32:40 -0300

    Versão 1.8.4.3

--- frontend/src/componentes/LayoutModel.tsx
+++ frontend/src/componentes/LayoutModel.tsx
@@ -271,7 +271,7 @@ export default function LayoutModel({ children, titulo }: LayoutModelProps) {
 
         {/* Rodapé */}
         <footer className="text-right text-gray-500 text-xs mt-4">
-          Versão 1.8.4.2
+          Versão 1.8.4.3
         </footer>
       </div>
     </div>
